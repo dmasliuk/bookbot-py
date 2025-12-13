ab86127c15fd137db76958ca27c701b32914fb5f
dmasliuk <dmasliuk.w@gmail.com>
2025-12-13 00:08:07 -0700

    add character sorting functionality(complited)

--- main.py
+++ main.py
@@ -2,8 +2,16 @@ from stats import get_book_text, count_book_words, count_chars, sort_chars
 
 def main():
     content = get_book_text("books/frankenstein.txt")
+    
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
     print(f"Found {count_book_words(content)} total words")
-    book_chars_count = count_chars(content)
-    print(sort_chars(book_chars_count))
+    print("--------- Character Count -------")
+    for char in sort_chars(count_chars(content)):
+        if char["char"].isalpha():
+            print(f"{char["char"]}: {char["num"]}")
+    print("============= END ===============")
 
+  
 main()
--- stats.py
+++ stats.py
@@ -16,17 +16,17 @@ def count_chars(content):
       content_dict[char] += 1
   return content_dict
 
-def get_key(dict):
-  return 
+def get_num_key(dict):
+  return dict["num"]
 
 def sort_chars(chars):
   sorted_chars = []
   
   for k, v in chars.items():
     char_dict = {
-    "char": k,
-    "num": v
+      "char": k,
+      "num": v
     }
     sorted_chars.append(char_dict)
-  sorted_chars = sorted(sorted_chars, key = lambda x: (x["num"]),reverse = True)
+  sorted_chars.sort(key = get_num_key, reverse = True) 
   return sorted_chars
